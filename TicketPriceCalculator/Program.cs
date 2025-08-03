cbdb7fc7f772fb5d78a090e833b0c44c03f87706
MC-CLINT <ckvidam@st.ug.edu.gh>
2025-08-03 03:18:24 +0000

    Program Built...Checking for errors

--- TicketPriceCalculator/Program.cs
+++ TicketPriceCalculator/Program.cs
@@ -10,7 +10,32 @@ namespace TicketPriceCalculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your age: ");
+            string input = Console.ReadLine();
 
+            // Validate input first
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Age Input cannot be empty.");
+
+            }
+
+            else if (int.TryParse(input, out int age))
+            {
+                if (age < 0 || age > 120)
+                {
+                    Console.WriteLine("Please enter your right age).");
+                }
+                else
+                {
+                    int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+                    Console.WriteLine($"Your ticket price is GHC{ticketPrice}.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid age using a whole number (eg., 25).");
+            }
         }
     }
 }
